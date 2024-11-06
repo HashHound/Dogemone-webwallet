cc85ac781c8623bc1ea73d8a7e4351f6e0c14bd2
HashHound <aravindkishore21@gmail.com>
2024-11-06 23:46:12 +0530

    Trying to make transactions smoother

---
+++ utils/helper.ts
@@ -0,0 +1,8 @@
+// utils/helper.ts
+export function debounce(func: Function, delay: number) {
+    let timer: NodeJS.Timeout;
+    return function (...args: any[]) {
+        clearTimeout(timer);
+        timer = setTimeout(() => func.apply(this, args), delay);
+    };
+}
